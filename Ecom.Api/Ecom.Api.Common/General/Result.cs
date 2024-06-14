3948684e18d6071a39b14bb9d02c1e1b5324ac1b
pranav <43924596+pranstahub@users.noreply.github.com>
2024-06-14 14:16:43 +0400

    Add files via upload

---
+++ Ecom.Api/Ecom.Api.Common/General/Result.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Api.Common.General
+{
+    public abstract class Result
+    {
+        public List<Errors> Errors { get; set; }
+        public bool IsError => Errors != null && Errors.Any();
+    }
+
+    public class Result<T> : Result
+    {
+        public T Response { get; set; }
+        public string WarningMessage { get; set; }
+    }
+}
